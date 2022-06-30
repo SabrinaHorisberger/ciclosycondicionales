a1866b3e2f8422c8c968a8833c4bcfa59924bef1
Sabrina Horisberger <sabrinahorisberger@gmail.com>
2022-06-29 23:45:15 -0300

    ciclos y condicionales

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,49 @@
 
-let desayuno = prompt("ingrese su desayuno")
-if(desayuno == "cafe"){
-    console.log("desayuno cafe con leche");
+
+
+
+
+
+
+// Algoritmo con condicionales:
+
+let PrefAlimenticias = prompt("Ingrese su preferencia Alimenticia:").toLowerCase()
+
+if(PrefAlimenticias == "vegetariana"){
+    console.log ("Los mejores lugares para comer comida vegetariana en Buenos Aires son:");
+} else if(PrefAlimenticias == "vegana") {
+    console.log("Los mejores lugares para comer comida vegana en Buenos Aires son:");
+} else if(PrefAlimenticias == " celiaca"){
+    console.log("Los mejores lugares para comer comida celiaca en Buenos Aires son:");
+} else if(PrefAlimenticias == "ninguna"){
+    console.log("Los mejores lugares para comer en Buenos Aires son:");
+} else {
+    alert("Por favor, ingrese una opción válida:");
+
+}
+
+let numero = parseInt(prompt( " Cuánto estas dispuesto a gastar?"))
+
+if( numero <= 3000 && isNaN){
+    console.log ("Los mejores lugares para comer por debajo de los $3000 son:");
+} else if(numero <= 5000){
+    console.log("Los mejores lugares para comer por debajo de los $5000 son:");
+} else if(numero >= 5000){
+    ("Por este rango de precios podes disfrutar una rica comida en:");
+} else{
+    alert(" Ingrese una opción válida")
+}
+
+//Algoritmo con ciclos
+
+let edad 
+
+do{
+    edad = parseFloat (prompt("Ingrese su edad, por favor:"))
+
+
+}while(isNaN(edad) || edad <=18){
+    alert("Puede ingresar")
+    
+   
 }
